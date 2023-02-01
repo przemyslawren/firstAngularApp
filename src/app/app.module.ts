508f211e86413b2dec30c0e150cf7d426460c577
Przemysław Ren <przemyslaw.ren@gmail.com>
2023-02-01 14:29:03 +0100

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,
+    ServerComponent,
+    ServersComponent, 
+    WarningAlertComponent, 
+    SuccessAlertComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-success-alert',
+    templateUrl: './success-alert.component.html',
+    styles: [`
+    p {color: darkgreen };
+    `],
+    
+})
+export class SuccessAlertComponent {
+}
